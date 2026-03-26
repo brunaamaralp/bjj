6e734edc010014d89657c36617d901a8c7bba815
BJJ-Manager <noreply@bjjmanager.local>
2026-03-26 09:43:34 -0300

    fix(auth): revert appwrite project id to correct value

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -2,7 +2,7 @@ import { Client, Account, Databases, Functions, Teams } from "appwrite";
 
 const endpoint = import.meta.env.VITE_APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1";
 const endpointFallback = import.meta.env.VITE_APPWRITE_ENDPOINT_FALLBACK || "";
-const project = import.meta.env.VITE_APPWRITE_PROJECT || "69ad727d001fdb4d65e6";
+const project = import.meta.env.VITE_APPWRITE_PROJECT || "699f020c00171ce26206";
 const selectedEndpoint = endpointFallback || endpoint;
 const client = new Client()
     .setEndpoint(selectedEndpoint)
